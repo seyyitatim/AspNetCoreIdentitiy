1f5e4b445b73120b1c8deab4beaf21eaf4df65aa
Seyyit <seyyitatim@gmail.com>
2021-06-30 18:38:54 +0300

    Add project files.

---
+++ Asp.NetCoreIdentity/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Asp.NetCoreIdentity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreIdentity.Data
+{
+    public class ApplicationDbContext:IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+        }
+
+    }
+}
