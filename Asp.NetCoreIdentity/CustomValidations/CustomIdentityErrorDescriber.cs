aad4b2d51e96af6854d6a21d4beab6608f2eb7cf
Seyyit <seyyitatim@gmail.com>
2021-07-01 19:44:34 +0300

    add custom identity error describer

---
+++ Asp.NetCoreIdentity/CustomValidations/CustomIdentityErrorDescriber.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreIdentity.CustomValidations
+{
+    public class CustomIdentityErrorDescriber:IdentityErrorDescriber
+    {
+
+        public override IdentityError InvalidUserName(string userName)
+        {
+            return new IdentityError()
+            {
+                Code = "InvalidUserName",
+                Description = $"Bu {userName} geçersizdir."
+            };
+        }
+
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return new IdentityError()
+            {
+                Code = "DuplicateEmail",
+                Description = $"Bu {email} kullanılmaktadır."
+            };
+        }
+
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description = $"Şifreniz en az {length} karakter olmalıdır."
+            };
+        }
+    }
+}
