0c5a7e6217ca015dbda3ab68d93b0d147a380242
Seyyit <seyyitatim@gmail.com>
2021-07-01 19:43:48 +0300

    add custom identity error decriber

---
+++ Asp.NetCoreIdentity/CustomValidations/CustomPasswordValidator.cs
@@ -0,0 +1,53 @@
+﻿using Asp.NetCoreIdentity.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreIdentity.CustomValidations
+{
+    public class CustomPasswordValidator : IPasswordValidator<AppUser>
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user, string password)
+        {
+            List<IdentityError> errors = new List<IdentityError>();
+
+            if (password.ToLower().Contains(user.UserName.ToLower()))
+            {
+                errors.Add(new IdentityError()
+                {
+                    Code = "PasswordContainsUserName",
+                    Description = "Şifre alanı kullanıcı adı içeremez"
+                });
+            }
+
+            if (password.ToLower().Contains("1234"))
+            {
+                errors.Add(new IdentityError()
+                {
+                    Code = "PasswordContains1234",
+                    Description = "Şifre alanı ardışık sayı içeremez"
+                });
+            }
+
+            if (password.ToLower().Contains(user.Email.ToLower()))
+            {
+                errors.Add(new IdentityError()
+                {
+                    Code = "PasswordContainsEmail",
+                    Description = "Şifre alanı email adresini içeremez"
+                });
+            }
+
+            if (errors.Count==0)
+            {
+                return Task.FromResult(IdentityResult.Success);
+            }
+            else
+            {
+                return Task.FromResult(IdentityResult.Failed(errors.ToArray()));
+            }
+        }
+    }
+}
