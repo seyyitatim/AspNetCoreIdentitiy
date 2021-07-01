1e3c84fe8417b74085eb48243926f5750f9abe2d
Seyyit <seyyitatim@gmail.com>
2021-07-01 19:43:20 +0300

    add login and sing up page.

---
+++ Asp.NetCoreIdentity/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using Asp.NetCoreIdentity.Entities;
+using Asp.NetCoreIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreIdentity.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("SingUp");
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SingUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SingUp(UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser();
+
+                user.UserName = model.UserName;
+                user.Email = model.Email;
+                user.PhoneNumber = model.PhoneNumber;
+
+                IdentityResult result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model); 
+        }
+    }
+}
